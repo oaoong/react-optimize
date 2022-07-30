abe3198acac3fce8a9631264d5711a938320526e
정재희 <pizzachicken@kakao.com>
2022-07-30 19:10:39 +0900

    video, font optimize

--- lecture-3-master/src/components/BannerVideo.js
+++ lecture-3-master/src/components/BannerVideo.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import video from '../assets/banner-video.mp4'
+import React, { useState, useEffect } from "react";
+import video_webm from "../assets/banner-video_webm.webm";
+import video from "../assets/banner-video.mp4";
+import FontFaceObserver from "fontfaceobserver";
 
 function BannerVideo() {
-	return (
-		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
-			<div className="absolute h-screen w-full left-1/2">
-				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
-			</div>
-			<div className="w-full h-full flex justify-center items-center">
-				<div className="text-white text-center">
-					<div className="text-6xl leading-none font-semibold">KEEP</div>
-					<div className="text-6xl leading-none font-semibold">CALM</div>
-					<div className="text-3xl leading-loose">AND</div>
-					<div className="text-6xl leading-none font-semibold">RIDE</div>
-					<div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
-				</div>
-			</div>
-		</div>
-	)
+  const [isFontLoaded, setisFontLoaded] = useState(true);
+  useEffect(() => {
+    const font = new FontFaceObserver("BMYEONSUNG");
+    font.load(null, 10000).then(function () {
+      console.log("My Family has loaded");
+      setisFontLoaded(true);
+    });
+  }, []);
+
+  return (
+    <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
+      <div className="absolute h-screen w-full left-1/2">
+        <video
+          className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={video_webm} type="video/webm" />
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      <div
+        className="w-full h-full flex justify-center items-center"
+        style={{
+          opacity: isFontLoaded ? 1 : 0,
+          transition: "opacity 0.3s ease",
+        }}
+      >
+        <div className="text-white text-center">
+          <div className="text-6xl leading-none font-semibold">KEEP</div>
+          <div className="text-6xl leading-none font-semibold">CALM</div>
+          <div className="text-3xl leading-loose">AND</div>
+          <div className="text-6xl leading-none font-semibold">RIDE</div>
+          <div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BannerVideo
+export default BannerVideo;
