05e7e204ff980badf260d91dfec3ea9bdbcd54d5
정재희 <pizzachicken@kakao.com>
2022-08-03 15:44:37 +0900

    lazyload, layoutshift

--- lecture-4-master/src/components/PhotoItem.js
+++ lecture-4-master/src/components/PhotoItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import LazyLoad from 'react-lazyload';
 import { useDispatch } from 'react-redux';
 import { showModal } from '../redux/imageModal';
 
@@ -12,16 +13,26 @@ function PhotoItem({ photo: { urls, alt } }) {
 
   return (
     <ImageWrap>
-      <Image src={urls.small} alt={alt} onClick={openModal} />
+      <LazyLoad offset={500}>
+        <Image src={urls.small} alt={alt} onClick={openModal} />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
